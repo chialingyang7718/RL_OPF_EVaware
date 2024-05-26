42ae23d62fead5eba8ff5ac21909c0987efe13f8
chialingyang7718 <chialingyang7718@gmail.com>
2024-05-26 17:15:38 +0200

    vectorize the env

--- Scripts/main.py
+++ Scripts/main.py
@@ -5,50 +5,64 @@ import gymnasium as gym
 
 #Import the registered environment
 import CustEnv
-# from environment.PowerGridEnv import PowerGrid
+
 
 #Import the grid loader
 from grid_loader import load_test_case_grid, load_simple_grid, load_simbench_grid
 
 #Import stable baselines stuff
 from stable_baselines3 import PPO
+from stable_baselines3 import common
 
 #Import other stuff
 import torch as th
 import os
 
 
-
-# Environment Setup
+# Load the grid
 # grid = load_test_case_grid(14)
 # grid = load_simple_grid()
 grid_code = "1-HV-urban--0-sw"
 grid = load_simbench_grid(grid_code)
+
+# Environment Setup
 # env = PowerGrid(grid, 0.03, 96, True, True) 
-env = gym.make('PowerGrid-v0', net = grid, dispatching_intervals = 96, num_envs = 2)
+if __name__ == '__main__':
+# initiate the environment for vectorizing
+    env_fns = [
+        lambda: gym.make(
+            'PowerGrid-v0', 
+            net = grid, 
+            dispatching_intervals = 2, 
+            UseDataSource = True, 
+            UseSimbench = True
+            )] * 2
 
+    # vectorize the environment
+    # env = gym.vector.AsyncVectorEnv(env_fns, shared_memory=True)
+    env = common.vec_env.SubprocVecEnv(env_fns, start_method=None)
 
-# create the log path
-log_path = os.path.join('Training', 'Logs')
+    # create the log path
+    log_path = os.path.join('Training', 'Logs')
 
-# custom MLP policy of two layers of size 256
-policy_kwargs = dict(
-    activation_fn=th.nn.Tanh, net_arch=dict(pi=[256, 256], vf=[256, 256])
-)
+    # custom MLP policy of two layers of size 256
+    policy_kwargs = dict(
+        activation_fn=th.nn.Tanh, net_arch=dict(pi=[256, 256], vf=[256, 256])
+    )
 
-# create the agent
-model = PPO(policy="MlpPolicy", 
-            env = env, 
-            n_steps = 100, 
-            verbose=1, 
-            policy_kwargs = policy_kwargs, 
-            tensorboard_log=log_path)
+    # create the agent
+    model = PPO(policy="MlpPolicy", 
+                env = env, 
+                n_steps = 2, 
+                verbose=1, 
+                policy_kwargs = policy_kwargs, 
+                tensorboard_log=log_path)
 
-# train the agent
-model.learn(total_timesteps= 60000, progress_bar=True)
+    # train the agent
+    model.learn(total_timesteps= 60, progress_bar=True)
 
-# save the model
-model.save("Training/Model/%s" % grid_code)
+    # save the model
+    # model.save("Training/Model/%s" % grid_code)
 
 
 # DONE: why the model does not end Ans: the n_steps was much larger than total_timesteps
--- Scripts/test_env.py
+++ Scripts/test_env.py
@@ -3,7 +3,7 @@ import gymnasium as gym
 
 # import the environment
 import CustEnv
-# import CustEnv.Env.PowerGridEnv as environment
+
 
 # import the grid loader
 from grid_loader import load_test_case_grid, load_simple_grid, load_simbench_grid
@@ -14,30 +14,28 @@ grid = load_test_case_grid(14)
 # grid_code = "1-HV-urban--0-sw"
 # grid = load_simbench_grid(grid_code)
 
+if __name__ == '__main__':
+    # initiate the environment for vectorizing
+    env_fns = [lambda: gym.make('PowerGrid-v0', net = grid, dispatching_intervals = 2)] * 2
+    # vectorize the environment
+    env = gym.vector.AsyncVectorEnv(env_fns, shared_memory=True)
+    terminated = False
+    truncated = False
 
-# initiate the environment
-# env = environment.PowerGrid(grid, 0.03, 2, False, False)
-env_fns = [lambda: gym.make('PowerGrid-v0', net = grid, dispatching_intervals = 2)] * 2
-# env = gym.make('PowerGrid-v0', net = grid, dispatching_intervals = 2)
-env = gym.vector.AsyncVectorEnv(env_fns, shared_memory=False)
 
-
-
-# test the environment by randomly selecting actions
-def test_env():
+    # test the environment by randomly selecting actions
+    # def test_env():
     episodes = 2
     for episode in range(1, episodes+1):
         state = env.reset()
-        terminated = False
-        truncated = False
-        score = 0 
-        while terminated == False and truncated == False:
-            env.render()
+        score = 0
+        action = env.action_space.sample()
+        n_state, reward, terminated, truncated, info = env.step(action)
+        while terminated.any() == False and truncated.any() == False:
             action = env.action_space.sample()
             n_state, reward, terminated, truncated, info = env.step(action)
             score+=reward
         print('Episode:{} Score:{}'.format(episode, score))
     env.close()
 
-if __name__ == '__main__':
-    test_env()
+
