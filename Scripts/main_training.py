f33a6e2397b4f484ef4f0e68328eaa947ea6ffcc
chialingyang7718 <chialingyang7718@gmail.com>
2024-09-09 10:38:47 +0200

    updated environment

--- Scripts/main_training.py
+++ Scripts/main_training.py
@@ -120,7 +120,7 @@ if __name__ == '__main__':
     # model.learn(total_timesteps= 96, callback=[soc_callback], progress_bar=True)
 
     # save the model
-    # model.save("Training/Model/Env_v1/Case%s_EV_3innerLayer" % n_case)
+    model.save("Training/Model/Env_v1/Case%s_EV_3innerLayer_EnvV1" % n_case)
 
  
 
