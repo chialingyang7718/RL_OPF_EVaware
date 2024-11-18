c95eb78b43656cab0e6ed2064026b4753789265d
chialingyang7718 <chialingyang7718@gmail.com>
2024-11-18 11:58:43 +0100

    patch: clean up the script

--- Scripts/main_training.py
+++ Scripts/main_training.py
@@ -9,9 +9,6 @@ import CustEnv
 # Import the grid loader
 from grid_loader import load_test_case_grid
 
-# Import the callback
-# from callback_soc import SOCCallback
-
 # Import stable baselines stuff
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import SubprocVecEnv
@@ -32,21 +29,18 @@ def make_env(env_id, net, dispatching_interval, Training, rank):
             env_id,
             net=net,
             dispatching_intervals=dispatching_interval,
-            # EVScenario=EVScenario,
             Training=Training,
         )
         env.reset(seed=rank)
         env = Monitor(env)
         return env
-
     return _init
 
-# find the nearest power of 2 to the number of buses
 def nearestPowerOf2(N):
     a = int(math.log2(N))
     if 2**a == N:
         return N
-    return 2 ** (a + 1)  # return the ceiling of the power of 2
+    return 2 ** (a + 1)
 
 def create_unique_soc_log_path(base_log_dir):
         # Ensure the base log directory exists
@@ -77,10 +71,8 @@ def create_unique_soc_log_path(base_log_dir):
 if __name__ == "__main__":
 
     # Load the grid
-    # n_case = int(input("Enter Test Case Number (6ww, 9, 14, 30, 33bw, 39, 57, 118...): "))
     n_case = 5
     grid = load_test_case_grid(n_case)
-    # grid = load_test_case_grid(n_case, "ww")
 
     # Parameters
     env_id = "PowerGrid-v2"
@@ -94,7 +86,6 @@ if __name__ == "__main__":
                 env_id,
                 net=grid,
                 dispatching_interval=24,
-                # EVScenario=EVScenario,
                 Training=Training,
                 rank=i,
             )
@@ -105,14 +96,10 @@ if __name__ == "__main__":
     # create the log path
     log_path = os.path.join("Training", "Logs", "Case%s" % (n_case))
 
-    # custom MLP policy: network depends on the observation space, indirectly, the number of buses
-
-
-    # choose a network size that is slightly larger than observation space
     NN_size = nearestPowerOf2(n_case * 4) 
     n_steps = 120
 
-    # the policy network architecture (if the number of buses is less than 20, 2 layers; otherwise, 3 layers)
+    # choose network architecture depending on the case number
     if n_case <= 20:
         n_layers = 2 + int(n_case/10)
         policy_kwargs = dict(
@@ -137,12 +124,6 @@ if __name__ == "__main__":
                     net_arch=dict(pi=[NN_size*4, NN_size*2, NN_size*2, NN_size*2, NN_size, NN_size], vf=[NN_size*4, int(NN_size/2), int(NN_size/4)]) 
                 )
         
-
-
-        # initialize callback
-        # soc_log_path = create_unique_soc_log_path(os.path.join(log_path, "SOC"))
-        # soc_callback = SOCCallback(log_dir=soc_log_path)
-
     # create checkpoint callback
     checkpoint_callback = CheckpointCallback(
         save_freq=20000,
@@ -150,10 +131,9 @@ if __name__ == "__main__":
         name_prefix="Case%s_model"%n_case,
         )
 
-    # create the agent or reload the model
-    if os.path.exists("Training/Model/Case%s/Case%s.zip" %(n_case, n_case)):    # check if the model exists:
+    # continue training or start new training
+    if os.path.exists("Training/Model/Case%s/Case%s.zip" %(n_case, n_case)):   
         model = PPO.load("Training/Model/Case%s/Case%s" %(n_case, n_case), env=env)
-        # Continue training the model
         model.learn(total_timesteps=200000, reset_num_timesteps=False, tb_log_name="PPO_continue", callback=checkpoint_callback, progress_bar=True)
 
     else:
@@ -167,30 +147,9 @@ if __name__ == "__main__":
             policy_kwargs=policy_kwargs,
             tensorboard_log=log_path,
         )
-        # total_timesteps = 100000 * (int(n_case/10) + 5)
         total_timesteps = 500000
-        # model.learn(total_timesteps=60, progress_bar=True)
         model.learn(total_timesteps=total_timesteps, callback=checkpoint_callback, progress_bar=True)
 
-
-    # save the model
     model.save("Training/Model/Case%s/Case%s" %(n_case, n_case))
     env.close()
 
-# DONE: why the model does not end -- Ans: the n_steps was much larger than total_timesteps
-# DONE: implement time series data for simple case
-# DONE: omit generator
-# DONE: implement simbench data -- changed the network architecture
-# DONE: include generation uncertainties into the observation -- added action limit at the step function
-# DONE: vectorize the environment -- register the env and make vectorized env
-# DONE: omit ext grid -- cannot omit it otherwise no reference bus is available
-# DONE: integrate EV charging: address negative power in storage -- need to add penalty for negative SOC
-# DONE: generate instances (loads and max. power generations) of the enironment - randomly generate the loads and max. power generations
-# DONE: exclude slack bus from generation cost since the slack bus may represent a large external grid or network that is assumed to have sufficient capacity without a direct cost tied to its power output
-# DONE: seperate the vlimit of generator and other buses
-
-# DONE: add phase angle difference limit into reward function
-# DONE: read voltage limit from the grid
-# TODO: output the report of divergence to the log fileS
-# TODO: choose a comparable benchmark: interior point method, etc.
-# TODO: implement safe reinforcement learning
